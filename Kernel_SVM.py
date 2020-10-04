4b4dac068ab079658e8ffa1a30664a4c2dbc6d79
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-04 08:42:58 -0600

    Import dataset

--- Kernel_SVM.py
+++ Kernel_SVM.py
@@ -7,4 +7,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import dataset
-
+dataset = pd.read_csv('Social_Network_ads.csv')
+X = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, -1].values
